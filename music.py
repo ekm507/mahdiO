1d4f28c7d943a41237f07d8d65351ae0aafc09be
Erfan Kheyrollahi <ekm507@gmail.com>
2019-12-13 02:02:29 +0330

    change the name notes to notes_listt

--- music.py
+++ music.py
@@ -1,16 +1,16 @@
 import notes
 
 
-def create_music(notes, sampling_rate):
+def create_music(notes_list, instrument, sampling_rate):
     note_duration = 4.0
     file_duration = 0
-    for n, duration in notes:
+    for n, duration in notes_list:
         file_duration += duration
     file_duration += note_duration
     audio_wave = [0 for i in range(int(file_duration * sampling_rate))]
     note_frame = 0
-    for number, duration in notes:
-        note = notes.create_note((number, note_duration))
+    for number, duration in notes_list:
+        note = notes.create_note((number, note_duration), instrument, sampling_rate)
         for i in range(len(note)):
             audio_wave[note_frame + i] += note[i]
         note_frame += int(duration * sampling_rate)
