279cbe46ff45545dd32906b3e42d7b2951bd3f18
Erfan Kheyrollahi <ekm507@gmail.com>
2019-12-13 01:52:34 +0330

    add instrument2

--- instruments.py
+++ instruments.py
@@ -1,4 +1,5 @@
 from functions import *
+import numpy as np
 
 # a simple music instrument, made using harmonics function.
 # main frequency is the music note frequency to be played
@@ -35,3 +36,29 @@ def push_instrument1(step):
         b = max_amp * np.exp(-step / 2) * np.abs(np.sin(step * 4) )
         return guard(b, min_amp, max_amp)
 
+def instrument2(main_freq, step):
+
+    harmonics_list=[
+        (1, 0.8),
+        (2, 1),
+        (3, 0.5),
+        (4, 0.4), 
+        (5, 0.3),
+        (6, 0.6),
+        (7, 0.1),
+        (8, 0.05)
+    ]
+
+    amplitude = push_instrument1(step)
+    return harmonics(np.sin, main_freq, harmonics_list, step) * amplitude
+
+def push_instrument2(step):
+    min_amp = 0
+    max_amp = 0.4
+    if step <=0:
+        b = max_amp
+    elif step <= 2 * np.pi:
+        b = max_amp * np.cos(step)
+    else:
+        b = 0
+    return guard(b, min_amp, max_amp)
