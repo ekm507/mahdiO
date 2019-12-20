ad0bd222bf8d96568e13c32b23053fbc885e3b31
Erfan Kheyrollahi <ekm507@gmail.com>
2019-12-20 06:30:52 +0330

    change midi to frequency

--- music.py
+++ music.py
@@ -1,5 +1,5 @@
 import notes
-
+from midi_conversions import midi2freq
 
 def create_music(notes_list, instrument, sampling_rate):
     note_duration = 4.0
@@ -9,8 +9,12 @@ def create_music(notes_list, instrument, sampling_rate):
     file_duration += note_duration
     audio_wave = [0 for i in range(int(file_duration * sampling_rate))]
     note_frame = 0
+    noteByFrequency = []
     for number, duration in notes_list:
-        note = notes.create_note((number, note_duration), instrument, sampling_rate)
+        noteByFrequency.append(tuple(midi2freq(number), duration))
+
+    for frequency, duration in noteByFrequency:
+        note = notes.create_note((frequency, note_duration), instrument, sampling_rate)
         for i in range(len(note)):
             audio_wave[note_frame + i] += note[i]
         note_frame += int(duration * sampling_rate)
--- notes.py
+++ notes.py
@@ -1,9 +1,7 @@
 import functions
 
 def create_note(note, instrument, sampling_rate):
-    number, duration = note
-    main_freq = 110.00
-    freq = main_freq * number
+    frequency, duration = note
     num_samples = int( sampling_rate * duration)
-    audio_wave = [instrument(freq, functions.pi_norm(x, sampling_rate)) for x in range(num_samples)]
+    audio_wave = [instrument(frequency, functions.pi_norm(x, sampling_rate)) for x in range(num_samples)]
     return audio_wave
