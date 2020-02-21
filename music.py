9e127f241ecaef2f8a005c5efc3b7c61da56e964
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2020-02-21 21:13:54 +0330

    add new note generation function with new standard(note-time) for notes list.

--- music.py
+++ music.py
@@ -1,5 +1,6 @@
 import notes
 from midi_conversions import midi2freq
+from midi_conversions import any2freq
 
 def create_music(notes_list, instrument, sampling_rate):
     note_duration = 4.0
@@ -20,3 +21,34 @@ def create_music(notes_list, instrument, sampling_rate):
         note_frame += int(duration * sampling_rate)
 
     return audio_wave
+
+
+# create music function compatible with another standard for notes list.
+#TODO: document this file
+
+def create_music2(notes_list, instrument, sampling_rate=48000, note_duration=4.0, note_type='midi'):
+    
+    # how long music will be
+    file_duration = max(list(k[1] for k in notes_list)) + note_duration
+
+    # generate a list as long as needed for music.
+    audio_wave = [0 for _ in range(int(file_duration * sampling_rate))]
+
+    # a set of notes by frequency (not midi or string)
+    notesSet = set(k[0] for k in notes_list)
+
+    # generate notes files
+    noteFiles = {
+        k:
+        notes.create_note(
+            tuple(
+                any2freq(k, note_type),
+                note_duration
+            ),
+            instrument,
+            sampling_rate)
+    for k in notesSet
+    } # what a messy line of code it was!
+
+    # now that notes are generated, we are to mix them.
+
