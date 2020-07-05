ecf1443f314ef0a3d84aeaf3fcdb68ccb3d3d059
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2020-07-06 02:01:11 +0430

    time scale is better now

--- read_midi.py
+++ read_midi.py
@@ -2,16 +2,21 @@ import mido
 
 def read_midi(filename:str) -> list:
     midiFile = mido.MidiFile(filename)
+    tempoRate = 1
+    tickTime = 1
+    time_scale = midiFile.ticks_per_beat * tempoRate
+    tempo = 500000 # default tempo
+    time_scale = mido.tick2second(1, midiFile.ticks_per_beat , tempo)
+
     notes_temp = dict()
     notes = []
-    time = 0
-    kk = set()
+
     for track in midiFile.tracks:
         time = 0
         for note in track:
             if note.type in ['note_on', 'note_off']:
                 #TODO: get true time scale from midi file data
-                time += note.time / 10000.0 * 3
+                time += note.time * time_scale
             if note.type == 'note_on':
                 notes_temp[note.note] = (time, note.velocity)
             elif note.type == 'note_off':
@@ -22,5 +27,10 @@ def read_midi(filename:str) -> list:
                     notes.append( (noteNumber, startTime, noteDuration, velocity) )
                 except KeyError:
                     pass
-    
+            elif note.type == 'set_tempo':
+                tempo = note.tempo
+                time_scale = mido.tick2second(tickTime, midiFile.ticks_per_beat , tempo)
+            # else:
+            #     print(note) # for debugging and deveeloping
+
     return notes
